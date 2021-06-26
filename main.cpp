2ad1f5b4fe5ed9bb2fe777dd9e262fa9efe23ee6
Inoki <veyx.shaw@gmail.com>
2021-06-26 16:53:09 +0200

    Add EFI availability detection

--- main.cpp
+++ main.cpp
@@ -1,12 +1,20 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QMessageBox>
 #include <qefientrystaticlist.h>
 
+bool qefi_is_available();
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    if (!qefi_is_available()) {
+        QMessageBox::critical(nullptr, QStringLiteral("Error"),
+                              QStringLiteral("Permission insufficient or no EFI environment"));
+        return 1;
+    }
 
     QEFIEntryStaticList *list = QEFIEntryStaticList::instance();
     list->load();
