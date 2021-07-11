628736588f379af6cfc6bb4acb722215a354dad1
Inoki <veyx.shaw@gmail.com>
2021-07-11 16:58:10 +0200

    Add permission check

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
-    if (!qefi_is_available()) {
+    if (!qefi_is_available() || !qefi_has_privilege()) {
         QMessageBox::critical(nullptr, QStringLiteral("Error"),
                               QStringLiteral("Permission insufficient or no EFI environment"));
         return 1;
