854a01baaad3e3ae824bf4ce3a03c1005dabfec1
Integral <integral@murena.io>
2023-11-24 12:06:59 +0800

    Fix broken window icon on Wayland

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    a.setDesktopFileName("qefientrymanager");
     a.setWindowIcon(QIcon("../cc.inoki.qefientrymanager.png"));
 
     if (!qefi_is_available() || !qefi_has_privilege()) {
