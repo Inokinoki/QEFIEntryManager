95c01849fc9fb52df60c8d1ed7306438ce5711ac
Inoki <veyx.shaw@gmail.com>
2024-11-20 11:32:18 +0100

    Add translation infra for Qt5

--- main.cpp
+++ main.cpp
@@ -1,7 +1,10 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QDebug>
 #include <QMessageBox>
+#include <QResource>
+#include <QTranslator>
 #include <qefientrystaticlist.h>
 #include <qefi.h>
 
@@ -11,7 +14,18 @@ int main(int argc, char *argv[])
     a.setDesktopFileName("qefientrymanager");
     a.setWindowIcon(QIcon("../cc.inoki.qefientrymanager.png"));
 
-    if (!qefi_is_available() || !qefi_has_privilege()) {
+    QTranslator translator;
+    if (translator.load(QLocale(), QStringLiteral("app"), QStringLiteral("_"), QStringLiteral(":/translations"), QStringLiteral(".qm")))
+    {
+        a.installTranslator(&translator);
+    }
+    else
+    {
+        qDebug() << "Failed to load translation";
+    }
+
+    if (!qefi_is_available() || !qefi_has_privilege())
+    {
         QMessageBox::critical(nullptr, QObject::tr("Error"),
                               QObject::tr("Permission insufficient or no EFI environment"));
         return 1;
