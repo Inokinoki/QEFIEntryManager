e35f78aff371f100368d1623baa37ee487a4df9c
Inoki <veyx.shaw@gmail.com>
2024-11-20 21:01:33 +0100

    Make it translatable (#32)
    * Add translation folder and automatic translations
    
    * Add translation infra for Qt5
    
    * Add qt linguistic tools
    
    * Attempt to fix Qt6 Linguistic tool
    
    * Attempt to set target directory to fix Qt6 Linguistic tool
    
    * Add translation for qt6 later than executable
    
    * Do not link another time for qt6
    
    * Clean up CI
    
    * Unify qt5 and qt6 command to include translations
    
    * Improve chinese translations
    
    * Fix Qt5 search in cmake

---
+++ translations/app_zh.ts
@@ -0,0 +1,318 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>BootEntryEditorDialog</name>
+    <message>
+        <location filename="../qefientryview.cpp" line="487"/>
+        <source>Add EFI Boot Entry</source>
+        <translation>添加 EFI 启动项</translation>
+    </message>
+</context>
+<context>
+    <name>EditorDialog</name>
+    <message>
+        <location filename="../qefiloadoptioneditorview.cpp" line="22"/>
+        <source>Add Device Path</source>
+        <translation>添加设备路径</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../mainwindow.cpp" line="7"/>
+        <source>EFI Entry Manager</source>
+        <translation>EFI 启动项管理器</translation>
+    </message>
+</context>
+<context>
+    <name>QEFIDPEditorView</name>
+    <message>
+        <location filename="../qefidpeditorview.cpp" line="166"/>
+        <source>Partition Num</source>
+        <translation>分区号</translation>
+    </message>
+    <message>
+        <location filename="../qefidpeditorview.cpp" line="171"/>
+        <source>Start</source>
+        <translation>开始</translation>
+    </message>
+    <message>
+        <location filename="../qefidpeditorview.cpp" line="176"/>
+        <source>Size</source>
+        <translation>大小</translation>
+    </message>
+    <message>
+        <location filename="../qefidpeditorview.cpp" line="181"/>
+        <source>Format</source>
+        <translation>格式</translation>
+    </message>
+    <message>
+        <location filename="../qefidpeditorview.cpp" line="186"/>
+        <source>Signature Type</source>
+        <translation>签名类型</translation>
+    </message>
+    <message>
+        <location filename="../qefidpeditorview.cpp" line="191"/>
+        <source>Signature</source>
+        <translation>签名</translation>
+    </message>
+    <message>
+        <location filename="../qefidpeditorview.cpp" line="213"/>
+        <source>File</source>
+        <translation>文件</translation>
+    </message>
+    <message>
+        <location filename="../qefidpeditorview.cpp" line="356"/>
+        <source>GPT</source>
+        <translation>GPT</translation>
+    </message>
+    <message>
+        <location filename="../qefidpeditorview.cpp" line="359"/>
+        <source>PCAT</source>
+        <translation>PCAT</translation>
+    </message>
+    <message>
+        <location filename="../qefidpeditorview.cpp" line="367"/>
+        <source>None</source>
+        <translation>无</translation>
+    </message>
+    <message>
+        <location filename="../qefidpeditorview.cpp" line="370"/>
+        <source>MBR</source>
+        <translation>MBR</translation>
+    </message>
+    <message>
+        <location filename="../qefidpeditorview.cpp" line="373"/>
+        <source>GUID</source>
+        <translation>GUID</translation>
+    </message>
+</context>
+<context>
+    <name>QEFIEntryDetailBriefView</name>
+    <message>
+        <location filename="../qefientrydetailview.cpp" line="17"/>
+        <source>Device Path instance:</source>
+        <translation>设备路径实例：</translation>
+    </message>
+    <message>
+        <location filename="../qefientrydetailview.cpp" line="22"/>
+        <source>Device Path %1 type:</source>
+        <translation>设备路径 %1 类型：</translation>
+    </message>
+</context>
+<context>
+    <name>QEFIEntryDetailView</name>
+    <message>
+        <location filename="../qefientrydetailview.cpp" line="57"/>
+        <source>Brief</source>
+        <translation>概要</translation>
+    </message>
+</context>
+<context>
+    <name>QEFIEntryView</name>
+    <message>
+        <location filename="../qefientryview.cpp" line="67"/>
+        <source>Move up</source>
+        <translation>上移</translation>
+    </message>
+    <message>
+        <location filename="../qefientryview.cpp" line="68"/>
+        <source>Move down</source>
+        <translation>下移</translation>
+    </message>
+    <message>
+        <location filename="../qefientryview.cpp" line="69"/>
+        <source>Make default</source>
+        <translation>设为默认</translation>
+    </message>
+    <message>
+        <location filename="../qefientryview.cpp" line="70"/>
+        <source>Set reboot</source>
+        <translation>设为重启</translation>
+    </message>
+    <message>
+        <location filename="../qefientryview.cpp" line="87"/>
+        <source>Timeout: %1 second(s)</source>
+        <translation>超时：%1 秒</translation>
+    </message>
+    <message>
+        <location filename="../qefientryview.cpp" line="89"/>
+        <location filename="../qefientryview.cpp" line="385"/>
+        <location filename="../qefientryview.cpp" line="550"/>
+        <source>Add</source>
+        <translation>添加</translation>
+    </message>
+    <message>
+        <location filename="../qefientryview.cpp" line="90"/>
+        <location filename="../qefientryview.cpp" line="388"/>
+        <location filename="../qefientryview.cpp" line="450"/>
+        <source>Import</source>
+        <translation>导入</translation>
+    </message>
+    <message>
+        <location filename="../qefientryview.cpp" line="91"/>
+        <source>Save</source>
+        <translation>保存</translation>
+    </message>
+    <message>
+        <location filename="../qefientryview.cpp" line="92"/>
+        <source>Reset</source>
+        <translation>重置</translation>
+    </message>
+    <message>
+        <location filename="../qefientryview.cpp" line="255"/>
+        <source>Reboot to </source>
+        <translation>重启到 </translation>
+    </message>
+    <message>
+        <location filename="../qefientryview.cpp" line="257"/>
+        <source>Do you want to reboot now?</source>
+        <translation>您想现在就重启吗？</translation>
+    </message>
+    <message>
+        <location filename="../qefientryview.cpp" line="341"/>
+        <source>Export failed</source>
+        <translation>导出失败</translation>
+    </message>
+    <message>
+        <location filename="../qefientryview.cpp" line="342"/>
+        <source>Data to export is empty.</source>
+        <translation>导出的数据为空。</translation>
+    </message>
+    <message>
+        <location filename="../qefientryview.cpp" line="346"/>
+        <source>Export Boot%1</source>
+        <translation>导出 Boot%1</translation>
+    </message>
+    <message>
+        <location filename="../qefientryview.cpp" line="370"/>
+        <source>Disable</source>
+        <translation>禁用</translation>
+    </message>
+    <message>
+        <location filename="../qefientryview.cpp" line="370"/>
+        <source>Enable</source>
+        <translation>启用</translation>
+    </message>
+    <message>
+        <location filename="../qefientryview.cpp" line="373"/>
+        <source>Delete</source>
+        <translation>删除</translation>
+    </message>
+    <message>
+        <location filename="../qefientryview.cpp" line="376"/>
+        <source>Export</source>
+        <translation>导出</translation>
+    </message>
+    <message>
+        <location filename="../qefientryview.cpp" line="379"/>
+        <source>Property</source>
+        <translation>属性</translation>
+    </message>
+    <message>
+        <location filename="../qefientryview.cpp" line="419"/>
+        <location filename="../qefientryview.cpp" line="435"/>
+        <location filename="../qefientryview.cpp" line="443"/>
+        <location filename="../qefientryview.cpp" line="454"/>
+        <location filename="../qefientryview.cpp" line="462"/>
+        <source>Import failed</source>
+        <translation>导入失败</translation>
+    </message>
+    <message>
+        <location filename="../qefientryview.cpp" line="420"/>
+        <location filename="../qefientryview.cpp" line="543"/>
+        <source>Data are invalidated.</source>
+        <translation>数据无效。</translation>
+    </message>
+    <message>
+        <location filename="../qefientryview.cpp" line="426"/>
+        <source>Choose an ID</source>
+        <translation>选择一个 ID</translation>
+    </message>
+    <message>
+        <location filename="../qefientryview.cpp" line="427"/>
+        <source>Hex value from 0 to FFFF</source>
+        <translation>0 到 FFFF 的十六进制值</translation>
+    </message>
+    <message>
+        <location filename="../qefientryview.cpp" line="436"/>
+        <location filename="../qefientryview.cpp" line="455"/>
+        <location filename="../qefientryview.cpp" line="555"/>
+        <source>The action is cancelled.</source>
+        <translation>操作已取消。</translation>
+    </message>
+    <message>
+        <location filename="../qefientryview.cpp" line="444"/>
+        <source>The chosen ID is invalidated.</source>
+        <translation>选择的 ID 无效。</translation>
+    </message>
+    <message>
+        <location filename="../qefientryview.cpp" line="451"/>
+        <location filename="../qefientryview.cpp" line="551"/>
+        <source>Do you want to override Boot%1?</source>
+        <translation>您想覆盖 Boot%1 吗？</translation>
+    </message>
+    <message>
+        <location filename="../qefientryview.cpp" line="463"/>
+        <location filename="../qefientryview.cpp" line="535"/>
+        <location filename="../qefientryview.cpp" line="563"/>
+        <source>Data might be invalidated.</source>
+        <translation>数据可能无效。</translation>
+    </message>
+    <message>
+        <location filename="../qefientryview.cpp" line="534"/>
+        <location filename="../qefientryview.cpp" line="542"/>
+        <location filename="../qefientryview.cpp" line="554"/>
+        <location filename="../qefientryview.cpp" line="562"/>
+        <source>Add failed</source>
+        <translation>添加失败</translation>
+    </message>
+</context>
+<context>
+    <name>QEFILoadOptionEditorView</name>
+    <message>
+        <location filename="../qefiloadoptioneditorview.cpp" line="75"/>
+        <source>ID:</source>
+        <translation>ID：</translation>
+    </message>
+    <message>
+        <location filename="../qefiloadoptioneditorview.cpp" line="78"/>
+        <source>Name:</source>
+        <translation>名称：</translation>
+    </message>
+    <message>
+        <location filename="../qefiloadoptioneditorview.cpp" line="81"/>
+        <source>Optional Data:</source>
+        <translation>可选数据：</translation>
+    </message>
+    <message>
+        <location filename="../qefiloadoptioneditorview.cpp" line="83"/>
+        <source>Add Device Path</source>
+        <translation>添加设备路径</translation>
+    </message>
+    <message>
+        <location filename="../qefiloadoptioneditorview.cpp" line="87"/>
+        <source>Clear Device Path</source>
+        <translation>清除设备路径</translation>
+    </message>
+    <message>
+        <location filename="../qefiloadoptioneditorview.cpp" line="121"/>
+        <source>Device Path:</source>
+        <translation>设备路径：</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../main.cpp" line="29"/>
+        <source>Error</source>
+        <translation>错误</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="30"/>
+        <source>Permission insufficient or no EFI environment</source>
+        <translation>权限不足或非 EFI 环境</translation>
+    </message>
+</context>
+</TS>
