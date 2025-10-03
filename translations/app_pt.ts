ce07ba25f3437d85bee5796f666c5cf7f65fcfdc
Bruno Gonçalves <bigbruno@gmail.com>
2025-10-03 12:50:38 -0300

    Translation into Portuguese and Spanish (#36)
    * Translation into Portuguese and Spanish
    
    * Add translations/app_es.ts and translations/app_pt.ts in CmakeList.txt

---
+++ translations/app_pt.ts
@@ -0,0 +1,318 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="pt_BR">
+<context>
+    <name>BootEntryEditorDialog</name>
+    <message>
+        <location filename="../qefientryview.cpp" line="487"/>
+        <source>Add EFI Boot Entry</source>
+        <translation>Adicionar Entrada de Boot EFI</translation>
+    </message>
+</context>
+<context>
+    <name>EditorDialog</name>
+    <message>
+        <location filename="../qefiloadoptioneditorview.cpp" line="22"/>
+        <source>Add Device Path</source>
+        <translation>Adicionar Caminho do Dispositivo</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../mainwindow.cpp" line="7"/>
+        <source>EFI Entry Manager</source>
+        <translation>Gerenciador de Entradas EFI</translation>
+    </message>
+</context>
+<context>
+    <name>QEFIDPEditorView</name>
+    <message>
+        <location filename="../qefidpeditorview.cpp" line="166"/>
+        <source>Partition Num</source>
+        <translation>Número da Partição</translation>
+    </message>
+    <message>
+        <location filename="../qefidpeditorview.cpp" line="171"/>
+        <source>Start</source>
+        <translation>Início</translation>
+    </message>
+    <message>
+        <location filename="../qefidpeditorview.cpp" line="176"/>
+        <source>Size</source>
+        <translation>Tamanho</translation>
+    </message>
+    <message>
+        <location filename="../qefidpeditorview.cpp" line="181"/>
+        <source>Format</source>
+        <translation>Formato</translation>
+    </message>
+    <message>
+        <location filename="../qefidpeditorview.cpp" line="186"/>
+        <source>Signature Type</source>
+        <translation>Tipo de Assinatura</translation>
+    </message>
+    <message>
+        <location filename="../qefidpeditorview.cpp" line="191"/>
+        <source>Signature</source>
+        <translation>Assinatura</translation>
+    </message>
+    <message>
+        <location filename="../qefidpeditorview.cpp" line="213"/>
+        <source>File</source>
+        <translation>Arquivo</translation>
+    </message>
+    <message>
+        <location filename="../qefidpeditorview.cpp" line="356"/>
+        <source>GPT</source>
+        <translation>GPT</translation>
+    </message>
+    <message>
+        <location filename="../qefidpeditorview.cpp" line="359"/>
+        <source>PCAT</source>
+        <translation>PCAT</translation>
+    </message>
+    <message>
+        <location filename="../qefidpeditorview.cpp" line="367"/>
+        <source>None</source>
+        <translation>Nenhum</translation>
+    </message>
+    <message>
+        <location filename="../qefidpeditorview.cpp" line="370"/>
+        <source>MBR</source>
+        <translation>MBR</translation>
+    </message>
+    <message>
+        <location filename="../qefidpeditorview.cpp" line="373"/>
+        <source>GUID</source>
+        <translation>GUID</translation>
+    </message>
+</context>
+<context>
+    <name>QEFIEntryDetailBriefView</name>
+    <message>
+        <location filename="../qefientrydetailview.cpp" line="17"/>
+        <source>Device Path instance:</source>
+        <translation>Instância do Caminho do Dispositivo:</translation>
+    </message>
+    <message>
+        <location filename="../qefientrydetailview.cpp" line="22"/>
+        <source>Device Path %1 type:</source>
+        <translation>Tipo do Caminho do Dispositivo %1:</translation>
+    </message>
+</context>
+<context>
+    <name>QEFIEntryDetailView</name>
+    <message>
+        <location filename="../qefientrydetailview.cpp" line="57"/>
+        <source>Brief</source>
+        <translation>Resumo</translation>
+    </message>
+</context>
+<context>
+    <name>QEFIEntryView</name>
+    <message>
+        <location filename="../qefientryview.cpp" line="67"/>
+        <source>Move up</source>
+        <translation>Mover para cima</translation>
+    </message>
+    <message>
+        <location filename="../qefientryview.cpp" line="68"/>
+        <source>Move down</source>
+        <translation>Mover para baixo</translation>
+    </message>
+    <message>
+        <location filename="../qefientryview.cpp" line="69"/>
+        <source>Make default</source>
+        <translation>Definir como padrão</translation>
+    </message>
+    <message>
+        <location filename="../qefientryview.cpp" line="70"/>
+        <source>Set reboot</source>
+        <translation>Definir reinicialização</translation>
+    </message>
+    <message>
+        <location filename="../qefientryview.cpp" line="87"/>
+        <source>Timeout: %1 second(s)</source>
+        <translation>Tempo limite: %1 segundo(s)</translation>
+    </message>
+    <message>
+        <location filename="../qefientryview.cpp" line="89"/>
+        <location filename="../qefientryview.cpp" line="385"/>
+        <location filename="../qefientryview.cpp" line="550"/>
+        <source>Add</source>
+        <translation>Adicionar</translation>
+    </message>
+    <message>
+        <location filename="../qefientryview.cpp" line="90"/>
+        <location filename="../qefientryview.cpp" line="388"/>
+        <location filename="../qefientryview.cpp" line="450"/>
+        <source>Import</source>
+        <translation>Importar</translation>
+    </message>
+    <message>
+        <location filename="../qefientryview.cpp" line="91"/>
+        <source>Save</source>
+        <translation>Salvar</translation>
+    </message>
+    <message>
+        <location filename="../qefientryview.cpp" line="92"/>
+        <source>Reset</source>
+        <translation>Redefinir</translation>
+    </message>
+    <message>
+        <location filename="../qefientryview.cpp" line="255"/>
+        <source>Reboot to </source>
+        <translation>Reiniciar para </translation>
+    </message>
+    <message>
+        <location filename="../qefientryview.cpp" line="257"/>
+        <source>Do you want to reboot now?</source>
+        <translation>Você deseja reiniciar agora?</translation>
+    </message>
+    <message>
+        <location filename="../qefientryview.cpp" line="341"/>
+        <source>Export failed</source>
+        <translation>Falha na exportação</translation>
+    </message>
+    <message>
+        <location filename="../qefientryview.cpp" line="342"/>
+        <source>Data to export is empty.</source>
+        <translation>Os dados para exportar estão vazios.</translation>
+    </message>
+    <message>
+        <location filename="../qefientryview.cpp" line="346"/>
+        <source>Export Boot%1</source>
+        <translation>Exportar Boot%1</translation>
+    </message>
+    <message>
+        <location filename="../qefientryview.cpp" line="370"/>
+        <source>Disable</source>
+        <translation>Desativar</translation>
+    </message>
+    <message>
+        <location filename="../qefientryview.cpp" line="370"/>
+        <source>Enable</source>
+        <translation>Ativar</translation>
+    </message>
+    <message>
+        <location filename="../qefientryview.cpp" line="373"/>
+        <source>Delete</source>
+        <translation>Excluir</translation>
+    </message>
+    <message>
+        <location filename="../qefientryview.cpp" line="376"/>
+        <source>Export</source>
+        <translation>Exportar</translation>
+    </message>
+    <message>
+        <location filename="../qefientryview.cpp" line="379"/>
+        <source>Property</source>
+        <translation>Propriedade</translation>
+    </message>
+    <message>
+        <location filename="../qefientryview.cpp" line="419"/>
+        <location filename="../qefientryview.cpp" line="435"/>
+        <location filename="../qefientryview.cpp" line="443"/>
+        <location filename="../qefientryview.cpp" line="454"/>
+        <location filename="../qefientryview.cpp" line="462"/>
+        <source>Import failed</source>
+        <translation>Falha na importação</translation>
+    </message>
+    <message>
+        <location filename="../qefientryview.cpp" line="420"/>
+        <location filename="../qefientryview.cpp" line="543"/>
+        <source>Data are invalidated.</source>
+        <translation>Os dados são inválidos.</translation>
+    </message>
+    <message>
+        <location filename="../qefientryview.cpp" line="426"/>
+        <source>Choose an ID</source>
+        <translation>Escolha um ID</translation>
+    </message>
+    <message>
+        <location filename="../qefientryview.cpp" line="427"/>
+        <source>Hex value from 0 to FFFF</source>
+        <translation>Valor hexadecimal de 0 a FFFF</translation>
+    </message>
+    <message>
+        <location filename="../qefientryview.cpp" line="436"/>
+        <location filename="../qefientryview.cpp" line="455"/>
+        <location filename="../qefientryview.cpp" line="555"/>
+        <source>The action is cancelled.</source>
+        <translation>A ação foi cancelada.</translation>
+    </message>
+    <message>
+        <location filename="../qefientryview.cpp" line="444"/>
+        <source>The chosen ID is invalidated.</source>
+        <translation>O ID escolhido é inválido.</translation>
+    </message>
+    <message>
+        <location filename="../qefientryview.cpp" line="451"/>
+        <location filename="../qefientryview.cpp" line="551"/>
+        <source>Do you want to override Boot%1?</source>
+        <translation>Você deseja sobrescrever o Boot%1?</translation>
+    </message>
+    <message>
+        <location filename="../qefientryview.cpp" line="463"/>
+        <location filename="../qefientryview.cpp" line="535"/>
+        <location filename="../qefientryview.cpp" line="563"/>
+        <source>Data might be invalidated.</source>
+        <translation>Os dados podem ser inválidos.</translation>
+    </message>
+    <message>
+        <location filename="../qefientryview.cpp" line="534"/>
+        <location filename="../qefientryview.cpp" line="542"/>
+        <location filename="../qefientryview.cpp" line="554"/>
+        <location filename="../qefientryview.cpp" line="562"/>
+        <source>Add failed</source>
+        <translation>Falha ao adicionar</translation>
+    </message>
+</context>
+<context>
+    <name>QEFILoadOptionEditorView</name>
+    <message>
+        <location filename="../qefiloadoptioneditorview.cpp" line="75"/>
+        <source>ID:</source>
+        <translation>ID:</translation>
+    </message>
+    <message>
+        <location filename="../qefiloadoptioneditorview.cpp" line="78"/>
+        <source>Name:</source>
+        <translation>Nome:</translation>
+    </message>
+    <message>
+        <location filename="../qefiloadoptioneditorview.cpp" line="81"/>
+        <source>Optional Data:</source>
+        <translation>Dados Opcionais:</translation>
+    </message>
+    <message>
+        <location filename="../qefiloadoptioneditorview.cpp" line="83"/>
+        <source>Add Device Path</source>
+        <translation>Adicionar Caminho do Dispositivo</translation>
+    </message>
+    <message>
+        <location filename="../qefiloadoptioneditorview.cpp" line="87"/>
+        <source>Clear Device Path</source>
+        <translation>Limpar Caminho do Dispositivo</translation>
+    </message>
+    <message>
+        <location filename="../qefiloadoptioneditorview.cpp" line="121"/>
+        <source>Device Path:</source>
+        <translation>Caminho do Dispositivo:</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../main.cpp" line="29"/>
+        <source>Error</source>
+        <translation>Erro</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="30"/>
+        <source>Permission insufficient or no EFI environment</source>
+        <translation>Permissão insuficiente ou ambiente EFI não encontrado</translation>
+    </message>
+</context>
+</TS>
